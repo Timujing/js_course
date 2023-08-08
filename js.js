cdb7f8f605df8ba282f6e284db51ad358bf2e9e7
timurka <tim.mol4anov2011@yandex.ru>
2023-08-08 11:44:39 +0300

    console project

---
+++ js.js
@@ -0,0 +1,15 @@
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+let movieGraduate = prompt('оценка фильма?', '');
+
+personalMovieDB.movies[lastMovie] = movieGraduate;
+console.log(personalMovieDB);
